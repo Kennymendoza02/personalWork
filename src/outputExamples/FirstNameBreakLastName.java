6641842fee899f83bde38c7ee999af6f707a4218
Kenny Mendoza <kennymendoza02@gmail.com>
2018-09-13 13:10:53 -0500

    Display first and last name with line break

---
+++ src/outputExamples/FirstNameBreakLastName.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class FirstNameBreakLastName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Kenny");
+		System.out.println("Mendoza");
+		
+	}
+
+}
