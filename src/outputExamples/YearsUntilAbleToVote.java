a844c19359d0421f279f49d735858cc64caba9f8
Kenny Mendoza <kennymendoza02@gmail.com>
2018-09-13 13:25:21 -0500

    Years until voting

---
+++ src/outputExamples/YearsUntilAbleToVote.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class YearsUntilAbleToVote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("2");
+		
+	}
+
+}
