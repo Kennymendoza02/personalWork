8bead5dd913a5be67af500d41ee5db550a234415
Kenny Mendoza <kennymendoza02@gmail.com>
2018-09-13 13:28:36 -0500

    Years until voting using ints

--- src/outputExamples/YearsUntilAbleToVote.java
+++ src/outputExamples/YearsUntilAbleToVote.java
@@ -5,8 +5,12 @@ public class YearsUntilAbleToVote {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		System.out.println("2");
+	int age = 17;
+	int voteAge = 18;
+	int remainingAge = voteAge - age;
+	
+
+	System.out.println(remainingAge);
 		
 	}
 
