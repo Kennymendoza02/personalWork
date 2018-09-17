16cd889a72f5ca96b2db0413a69229008771b914
Kenny Mendoza <kennymendoza02@gmail.com>
2018-09-17 15:10:04 -0500

    Changed Mailing Label to accept keyboard input and made variable declaration class

--- src/outputExamples/MailingLabel.java
+++ src/outputExamples/MailingLabel.java
@@ -1,11 +1,20 @@
 package outputExamples;
 
+import java.util.Scanner;
+
 public class MailingLabel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("enter an integer");
+		int myint = keyboard.nextInt();
+		
+		
 		System.out.println("Kenny\n18495 Pleasant Street\nBrookfield, WI, 53045");
+		
+		System.out.println(myint);
 
 	}
 
