db490a899b672b1623209b2ff5820b6481031c94
Kenny <kennymendoza02@gmail.com>
2018-09-18 13:12:14 -0500

    Changed Mailing label to accept inputs and print out inputs with line breaks

--- src/outputExamples/MailingLabel.java
+++ src/outputExamples/MailingLabel.java
@@ -1,11 +1,32 @@
 package outputExamples;
 
+import java.util.Scanner;
+
 public class MailingLabel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Kenny\n18495 Pleasant Street\nBrookfield, WI, 53045");
+		
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+	
+		String firstName;
+		String streetAddress;
+		String csz;
+		
+		//First name
+		System.out.println("Enter First Name");
+		firstName = input.nextLine();
+		
+		//Street address
+		System.out.println("Enter Street Address");
+		streetAddress = input.nextLine();
+		
+		//City, State, and ZIP
+		System.out.println("Enter City, State, and ZIP Code");
+		csz = input.nextLine();
+		
+		System.out.println(firstName + System.lineSeparator() + streetAddress + System.lineSeparator() + csz);
 
 	}
 
