04244367d9cdb234bc512d06b5c72baaefa1830d
Kenny Mendoza <kennymendoza02@gmail.com>
2018-09-19 15:01:54 -0500

    Input height of monkey and output it

---
+++ src/inputExamples/Monkey.java
@@ -0,0 +1,23 @@
+package inputExamples;
+
+import java.util.Scanner;
+
+public class Monkey {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		
+		String adultMonkeyHeight;
+		
+		System.out.println("What is the height of an adult monkey?");
+		
+		adultMonkeyHeight = input.nextLine();
+		
+		System.out.println(adultMonkeyHeight);
+		
+		
+	}
+
+}
