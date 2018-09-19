8d08a3b7e4da9e35e80f4a9b26f67d3d49dc25b9
Kenny <kennymendoza02@gmail.com>
2018-09-19 11:00:01 -0500

    Made WritingCode1 and WritingCode2

---
+++ src/outputExamples/WriteCode1.java
@@ -0,0 +1,16 @@
+package outputExamples;
+
+public class WriteCode1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final int MAXSTUDENTS = 30;
+		final char middleInitial = 'P';
+		int numStudents = 0;
+		
+		System.out.println(MAXSTUDENTS);
+		
+	}
+
+}
---
+++ src/outputExamples/WriteCode2.java
@@ -0,0 +1,17 @@
+package outputExamples;
+
+public class WriteCode2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		final double MYSTERYNUMBER = 13.33;
+		int dead = 1234567;
+		final char MIDDLEINITIAL = 'Z';
+		
+		System.out.println(MYSTERYNUMBER);
+		System.out.println(MIDDLEINITIAL);
+		
+	}
+
+}
