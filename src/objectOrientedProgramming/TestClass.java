1b3c1374d961ee365cee2e4c767b90b95cb0b831
Kenny Mendoza <kennymendoza02@gmail.com>
2018-10-03 14:37:52 -0500

    Made room area calculator and tested

--- src/objectOrientedProgramming/TestClass.java
+++ src/objectOrientedProgramming/TestClass.java
@@ -5,17 +5,9 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Greeter bob = new Greeter();
-		bob.sayHello();
-		
-		Friend john = new Friend();
-		john.sayMessage();
-		
-		bob.sayHello();
-		bob.sayHello();
-		bob.sayHello();
-		john.sayMessage();
-		bob.sayHello();
+		Room oesterToasterRoom = new Room();
+		oesterToasterRoom.area();
+		oesterToasterRoom.areaWithDoor();
 	}
 
 }
