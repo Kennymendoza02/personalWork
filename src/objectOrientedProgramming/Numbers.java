25d17117458bdc50dc42f8ef99ae497fdd008fcd
Kenny Mendoza <kennymendoza02@gmail.com>
2018-10-04 14:09:29 -0500

    House Numbers and Test Class

---
+++ src/objectOrientedProgramming/Numbers.java
@@ -0,0 +1,21 @@
+package objectOrientedProgramming;
+
+public class Numbers {
+
+	public Numbers()
+	{
+	}
+	
+	public void sayNumber(double num)
+	{
+		System.out.println("Your number is: " + num);
+	}
+	public void sayNumberPlus2(double num2)
+	{
+		System.out.println("Your number is: " + (num2 + 2));
+	}
+	public void saySum(double num, double num2)
+	{
+		System.out.println("Your number is: " + (num + num2));
+	}
+}
---
+++ src/objectOrientedProgramming/NumbersTestClass.java
@@ -0,0 +1,15 @@
+package objectOrientedProgramming;
+
+public class NumbersTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Numbers OesterToaster = new Numbers();
+		OesterToaster.sayNumber(7.5);
+		OesterToaster.sayNumberPlus2(2001);
+		OesterToaster.saySum(2001,34);
+	
+	}
+
+}
