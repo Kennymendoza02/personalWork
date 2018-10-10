a929b74f41b78d33a811744cef743457bd3e66bc
Kenny Mendoza <kennymendoza02@gmail.com>
2018-10-10 15:08:30 -0500

    Car class

---
+++ src/objectOrientedProgramming/Car2.java
@@ -0,0 +1,19 @@
+package objectOrientedProgramming;
+
+public class Car2 {
+
+	int mpg;
+	int fuelTC;
+	int fuel;
+	int odometer;
+	
+	public Car()
+	{
+		mpg = 20;
+		fuelTC = 20;
+		fuel = fuelTC;
+		odometer = 0;
+	}
+	
+	public int 
+}
