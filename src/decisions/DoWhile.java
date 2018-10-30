7d27e1b1a0aef468b6084a660d12ef7f05653d01
Kenny Mendoza <kennymendoza02@gmail.com>
2018-10-30 15:09:02 -0500

    Nested loops

--- src/decisions/DoWhile.java
+++ src/decisions/DoWhile.java
@@ -44,6 +44,79 @@ public class DoWhile {
 			for(counter = 1; counter < 11; counter++){
 				System.out.println(numberOfHellos + " Hello");
 			}
+				break;
+			
+		case 4: 
+				int dong = 0;
+				int counter1 = 0;
+				for(counter1 = 1; counter1 < 11; counter1++){
+					dong = counter1;
+					dong = dong + counter1;
+					
+				}
+				System.out.println(dong);
+				break;
+					
+		case 5:
+			System.out.println("Please enter a number");
+			int numba = input.nextInt();
+			int count = 0;
+			int sum = 0;
+			for(count = 0; count < 5; count++){
+				sum = sum + numba;
+			}
+			System.out.println(sum);
+				break;
+			
+		case 6:
+			for(int column = 1; column <= 5; column++){
+				
+				for(int row = column; row <=6; row++){
+					if(row==6){
+						System.out.print(" " + column + " ");
+					} else {
+						System.out.print(" " + row + " ");
+					}
+				}System.out.println();
+			}
+			break;
+			
+		case 7:
+			System.out.println("Enter a number to go through prime factorization");
+			int numero = input.nextInt();
+			int factor2 = 0;
+			for(int factor = 2; factor<=7; factor = factor++){
+				
+				
+					if (numero % factor == 0){
+						factor2 = numero / factor;
+						System.out.println("(" + factor + ")" + "(" + factor2 + ")");
+				}
+			}
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 		}
 	}
 
