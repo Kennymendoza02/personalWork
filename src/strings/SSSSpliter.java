65bc07252a0d1dcdc5492ae1c4bc1960c4bae935
Kenneth Mendoza <kennymendoza02@gmail.com>
2018-11-26 13:50:58 -0600

    Social Security Number Splitter

--- src/strings/SSSSpliter.java
+++ src/strings/SSSSpliter.java
@@ -8,16 +8,22 @@ public class SSSSpliter {
 		// TODO Auto-generated method stub
 
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Enter your SSN: ");
 		String ssn = input.nextLine();
-		
+
 		ssn.replaceAll("-", "");
 		ssn.replaceAll(" ", "");
-		
-		System.out.println(ssn.substring(0, 2));
-		System.out.println(ssn.substring(3, 4));
-		System.out.println(ssn.substring(5, 8));
+
+		if (ssn.length() == 11) {
+			System.out.println(ssn.substring(0, 3));
+			System.out.println(ssn.substring(4, 6));
+			System.out.println(ssn.substring(7, 11));
+		} else if (ssn.length() == 9) {
+			System.out.println(ssn.substring(0, 3));
+			System.out.println(ssn.substring(3, 5));
+			System.out.println(ssn.substring(5, 9));
+		}
 	}
 
 }
